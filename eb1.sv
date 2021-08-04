1b6e69850a41370417d51233c8fc75c8e40bb0bf
Ameet Gohil <qubits@email.com>
2021-08-04 00:17:25 -0700

    add formal for eb1

--- eb1.sv
+++ eb1.sv
@@ -28,4 +28,48 @@ module eb1 #(DWIDTH=32)
       end // else: !if(~rstf)
    end // always @ (posedge clk or negedge rstf)
 
+`ifdef FORMAL
+   reg f_past_valid = 0;
+   initial assume(!rstf);
+   always @(posedge clk) begin
+      f_past_valid <= 1;
+      //reset state
+      if(f_past_valild && !rstf) begin
+         assert(i_data == 0);
+         assert(i_valid == 0);
+      end
+
+      if(f_past_valid && rstf && $past(rstf))
+        if($past(i_ready) && $past(t_valid))
+          assert(i_data == $past(t_data));
+      
+      if(f_past_valid && rstf && $past(rstf)) begin
+
+         if($past(t_valid))
+           assert(i_valid);
+         // i_valid should hold ready was not high in the past cycle
+         if($past(i_valid) && !$past(i_ready))
+           assert(i_valid);
+
+         // data value should not change
+         if($past(i_valid) && !$past(i_ready))
+           assert(i_data == $past(i_data));
+
+         // t_ready should only low if there is already data in the buffer and i_ready is low
+         if(i_ready)
+           assert(t_ready);
+         if(i_valid && !i_ready)
+           assert(t_ready == 0);
+
+         if(!i_valid)
+           assert(t_ready);
+
+
+      end
+      
+   end
+
+`endif
+
+   
 endmodule // eb1
